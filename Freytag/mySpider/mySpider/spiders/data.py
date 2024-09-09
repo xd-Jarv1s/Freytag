56dd5274ccbdc3948e481a15eaf4ef6ce9fee7f3
xd-Jarv1s <116846800+xd-Jarv1s@users.noreply.github.com>
2024-09-09 11:44:21 +0200

    Update data.py

--- Freytag/mySpider/mySpider/spiders/data.py
+++ Freytag/mySpider/mySpider/spiders/data.py
@@ -1,10 +1,6 @@
 import json
 import re
-
-# Function to extract specific patterns from text
-def extract_and_format(text, pattern):
-    matches = re.findall(pattern, text)
-    return [match for match in matches]
+import requests
 
 # Load the JSON file
 with open('dataClean.json', 'r') as file:
@@ -12,18 +8,51 @@ with open('dataClean.json', 'r') as file:
 
 # Access the specific data
 specific_data = data[0]["data"][15]
+# print(specific_data)
 
 # Define regex patterns
-patterns = {
-   "dates": r'"Datum":"(\d{2}\.\d{2}\.\d{4})"',
-   "events": r'"category":"([a-zA-Z\s]+)"',
-}
+date_pattern = r'"Datum":"(\d{2}\.\d{2}\.\d{4})"'
+slug_pattern = r'"slug":"([^"]+)"'
+id_pattern = r'"_id":"([^"]+)"'
+pattern_title = r'"title":"([a-zA-Z\s]+)"'
 
 # Extract data based on patterns
-extracted_data = {key: extract_and_format(specific_data, pattern) for key, pattern in patterns.items()}
+dates = re.findall(date_pattern, specific_data)
+slugs = re.findall(slug_pattern, specific_data)
+ids = re.findall(id_pattern, specific_data)
+titles = re.findall(pattern_title, specific_data)
 
-# Combine and format the results
-combined_results = extracted_data["dates"] + extracted_data["events"]
+# Ensure all lists have the same length
+min_length = min(len(dates), len(slugs), len(ids), len(titles))
 
-# Output the combined results and the link
-print(specific_data)
+# Initialize index
+index = 0
+
+# Use a while loop to process each set of values
+while index < min_length:
+    date = dates[index]
+    slug = slugs[index]
+    id_ = ids[index]
+    title = titles[index]
+
+    # Construct the output URL
+    link = f"https://frey-tag.at/kalender/{slug}?s={id_}"
+
+    # Print the combined results and the link
+    print(f"Title: {title}")
+    print(f"Date: {date}")
+    print(f"Link: {link}")
+    print("-----------")
+
+    # Increment the index
+    index += 1
+
+    # Your Telegram bot token
+    token = '7162097876:AAE27cvUGt6tUzuX3NI9VoNnoUsbNYYnBUM'
+    method = "sendMessage"
+
+    # Make a POST request to the Telegram API
+    response = requests.post(
+        url='https://api.telegram.org/bot{0}/{1}'.format(token, method),
+        data={'chat_id': -1002423350705, 'text': f"{title}\nDate: {date}\nLink: {link}"}
+    ).json()
